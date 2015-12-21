0792ba2772da76a3f1c21f86d259484984b19020
alberto <dr.kidb@gmail.com>
2015-12-21 15:39:56 -0600

    se sincronizan los eventos de la presentacion

--- assets/js/keyUp.js
+++ assets/js/keyUp.js
@@ -1,10 +1,5 @@
 $(document).keyup(function (e) {
-    if (e.keyCode === 39 || e.keyCode === 38 || e.keyCode === 13) {
-        console.log("siguiente");
-    }
-    if (e.keyCode === 40 || e.keyCode === 37) {
-        console.log("regresar");
-    }
+
     var slides = {
         uri:
                 [
@@ -14,4 +9,37 @@ $(document).keyup(function (e) {
                 ]
 
     }
+
+    if (e.keyCode === 39 || e.keyCode === 38 || e.keyCode === 13) {
+        console.log("siguiente");
+
+
+        if (slides.uri.length > keyUp_i) {
+            keyUp_i++                       
+        }
+        
+         console.log(slides.uri[keyUp_i]);
+         console.log(keyUp_i);
+         window.location =slides.uri[keyUp_i];
+    }
+    if (e.keyCode === 40 || e.keyCode === 37) {
+        console.log("regresar");
+        
+        if (keyUp_i > 0) {
+
+            keyUp_i--;
+        }
+       console.log(slides.uri[keyUp_i]);
+         console.log(keyUp_i);
+         window.location =slides.uri[keyUp_i];
+    }
+    
+   
 });
+var keyUp_i = 0;
+
+
+function setUriPosition(pos) {
+
+    keyUp_i = pos;
+}
